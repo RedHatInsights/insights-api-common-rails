39a64f9d643d2ce4cd5604bb8378bd624967cee8
Drew Bomhof <dbomhof@redhat.com>
2019-01-25 14:10:14 -0500

    Pulled out encode, decode methods
    Moved to use Thread.current instead as it allows
    validation before setting the setter

--- lib/manageiq/api/common.rb
+++ lib/manageiq/api/common.rb
@@ -1,8 +1,8 @@
 require "manageiq/api/common/engine"
+require "manageiq/api/common/headers"
 require "manageiq/api/common/inflections"
 require "manageiq/api/common/logging"
 require "manageiq/api/common/metrics"
-require "manageiq/api/common/headers"
 
 module ManageIQ
   module API
--- lib/manageiq/api/common/headers.rb
+++ lib/manageiq/api/common/headers.rb
@@ -1,42 +1,19 @@
-module HeaderScope
-  thread_mattr_accessor :current
-end
-
 module ManageIQ
   module API
     module Common
       module Headers
-
         def self.current=(val)
           validate_headers(val)
-          HeaderScope.current = val
+          Thread.current[:attr_current_headers] = val
         end
 
         def self.current
-          HeaderScope.current
+          Thread.current[:attr_current_headers]
         end
 
-        def self.decode(headers, key)
-          validate_headers(headers)
-          val = headers.instance_variable_get(:@req).stringify_keys
-          JSON.parse(Base64.decode64(val[key]))
-        end
-
-        def self.encode(val, key)
-          if val.is_a?(Hash)
-            hashed = val.stringify_keys
-            Base64.strict_encode64(hashed[key].to_json)
-          else
-            raise StandardError, "Must be a Hash"
-          end
-        end
-
-        def self.validate_headers(val)
-          if val.is_a?(ActionDispatch::Http::Headers)
-            true
-          else
-            raise StandardError, 'Not an ActionDispatch::Http::Headers Class'
-          end
+        private_class_method def self.validate_headers(val)
+          raise ArgumentError, 'Not an ActionDispatch::Http::Headers Class' unless val.is_a?(ActionDispatch::Http::Headers)
+          true
         end
       end
     end
