f09c5a09aec4c196ec39b10165859a32ab7a9c78
Ladislav Smola <lsmola@redhat.com>
2019-01-25 11:47:35 +0100

    Reuse encrypted columns set

--- lib/manageiq/api/common/open_api/serializer.rb
+++ lib/manageiq/api/common/open_api/serializer.rb
@@ -2,13 +2,13 @@ module OpenApi
   module Serializer
     def as_json(arg = {})
       previous = super
-      encryption_filtered = previous.except(*self.class.try(:encrypted_columns))
+      encrypted_columns_set = (self.class.try(:encrypted_columns) || []).to_set
+      encryption_filtered = previous.except(*encrypted_columns_set)
       return encryption_filtered unless arg.key?(:prefixes)
       /\/v(?<major>\d+)[x\.]?(?<minor>\d+)?\// =~ arg[:prefixes].first
       version = [major, minor].compact.join(".")
       schema  = Api::Docs[version].definitions[self.class.name]
       attrs   = encryption_filtered.slice(*schema["properties"].keys)
-      encrypted_columns_set = (self.class.try(:encrypted_columns) || []).to_set
 
       schema["properties"].keys.each do |name|
         attrs[name] = attrs[name].iso8601 if attrs[name].kind_of?(Time)
