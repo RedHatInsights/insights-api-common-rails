5b42bec69f782d087154ac73ced100656f87534b
Brandon Dunne <brandondunne@hotmail.com>
2019-02-06 12:15:32 -0500

    Add tests for the version string regex

--- lib/manageiq/api/common/open_api/serializer.rb
+++ lib/manageiq/api/common/open_api/serializer.rb
@@ -5,17 +5,20 @@ module OpenApi
       encrypted_columns_set = (self.class.try(:encrypted_columns) || []).to_set
       encryption_filtered = previous.except(*encrypted_columns_set)
       return encryption_filtered unless arg.key?(:prefixes)
-      /\A\/?(?<api>\w+)\/v(?<major>\d+)[x\.]?(?<minor>\d+)?\// =~ arg[:prefixes].first
-      version = [major, minor].compact.join(".")
+      version = api_version_from_prefix(arg[:prefixes].first)
       schema  = Api::Docs[version].definitions[self.class.name]
       attrs   = encryption_filtered.slice(*schema["properties"].keys)
-
       schema["properties"].keys.each do |name|
         attrs[name] = attrs[name].iso8601 if attrs[name].kind_of?(Time)
         attrs[name] = attrs[name].to_s if name.ends_with?("_id") || name == "id"
         attrs[name] = self.public_send(name) if !attrs.key?(name) && !encrypted_columns_set.include?(name)
       end
       attrs.compact
+    end
+
+    def api_version_from_prefix(prefix)
+      /\/?\w+\/v(?<major>\d+)[x\.]?(?<minor>\d+)?\// =~ prefix
+      [major, minor].compact.join(".")
     end
   end
 end
