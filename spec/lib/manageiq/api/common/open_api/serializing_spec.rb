b93d0c97851b2c6784f1a63ff770facc903d5d72
Brandon Dunne <brandondunne@hotmail.com>
2019-02-06 12:15:32 -0500

    Add tests for the version string regex

--- spec/lib/manageiq/api/common/open_api/serializing_spec.rb
+++ spec/lib/manageiq/api/common/open_api/serializing_spec.rb
@@ -1,7 +1,7 @@
 RSpec.describe("Serializing ApplicationRecord instances to JSON") do
-  context "Encrypted attributes are not in the JSON response" do
-    let(:base_class) { Class.new.tap { |c| c.prepend(OpenApi::Serializer) } }
+  let(:base_class) { Class.new.tap { |c| c.prepend(OpenApi::Serializer) } }
 
+  context "Encrypted attributes are not in the JSON response" do
     it "on a model that has encrypted columns" do
       model = Class.new(base_class) do
         def self.encrypted_columns
@@ -16,7 +16,7 @@ RSpec.describe("Serializing ApplicationRecord instances to JSON") do
       expect(model.new.as_json).to eq("a" => 1, "b" => 2)
     end
 
-    it "on a model that has encrypted columns" do
+    it "on a model that does not have encrypted columns" do
       model = Class.new(base_class) do
         def to_hash
           {"a" => 1, "b" => 2, "secret" => "value"}
@@ -25,5 +25,14 @@ RSpec.describe("Serializing ApplicationRecord instances to JSON") do
 
       expect(model.new.as_json).to eq("a" => 1, "b" => 2, "secret" => "value")
     end
+  end
+
+  it "properly detects the version to serialize for" do
+    expect(base_class.new.api_version_from_prefix("api/v0.0/")).to eq("0.0")
+    expect(base_class.new.api_version_from_prefix("api/v0.0/something")).to eq("0.0")
+    expect(base_class.new.api_version_from_prefix("api/v0.1/something")).to eq("0.1")
+    expect(base_class.new.api_version_from_prefix("/api/v0.1/something")).to eq("0.1")
+    expect(base_class.new.api_version_from_prefix("a/b/v/v0.1/something")).to eq("0.1")
+    expect(base_class.new.api_version_from_prefix("/a/b/v/v0.1/something")).to eq("0.1")
   end
 end
