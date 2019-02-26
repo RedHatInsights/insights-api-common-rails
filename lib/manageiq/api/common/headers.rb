5c7d6bd7274319c5bf5a34ea5cb6ff80dcee861b
Madhu Kanoor <mkanoor@redhat.com>
2019-02-26 14:11:26 -0500

    Support for forwardable headers
    When service portal or approval service needs to communicate
    with other services, it needs to forward certain headers to the
    other services.

--- lib/manageiq/api/common/headers.rb
+++ lib/manageiq/api/common/headers.rb
@@ -2,6 +2,7 @@ module ManageIQ
   module API
     module Common
       module Headers
+        FORWARDABLE_HEADER_KEYS = %w(X-Request-ID x-rh-identity)
         def self.current=(val)
           validate_headers(val)
           Thread.current[:attr_current_headers] = val
@@ -9,6 +10,11 @@ module ManageIQ
 
         def self.current
           Thread.current[:attr_current_headers]
+        end
+
+        def self.forwardable_headers
+          raise "Current Headers has not been set" unless Thread.current[:attr_current_headers]
+          Thread.current[:attr_current_headers].to_h.slice(*FORWARDABLE_HEADER_KEYS)
         end
 
         private_class_method def self.validate_headers(val)
--- spec/lib/manageiq/api/common/headers_spec.rb
+++ spec/lib/manageiq/api/common/headers_spec.rb
@@ -1,5 +1,5 @@
 describe ManageIQ::API::Common::Headers do
-  let(:header_good) { ActionDispatch::Http::Headers.new({:blah => 'blah'}) }
+  let(:header_good) { ActionDispatch::Http::Headers.from_hash(:blah => 'blah', 'x-rh-identity' => 'abc') }
   let(:header_bad)  { {:blah => 'blah' } }
   context "set current headers" do
     it 'sets if the class is correct' do
@@ -8,6 +8,14 @@ describe ManageIQ::API::Common::Headers do
 
     it 'raises an exception if the class is incorrect' do
       expect { ManageIQ::API::Common::Headers.current = header_bad }.to raise_exception(ArgumentError)
+    end
+  end
+
+  context "get forwardable headers" do
+    it "x-rh-identity" do
+      ManageIQ::API::Common::Headers.current = header_good
+      hash = ManageIQ::API::Common::Headers.forwardable_headers
+      expect(hash['x-rh-identity']).to eq('abc')
     end
   end
 end
