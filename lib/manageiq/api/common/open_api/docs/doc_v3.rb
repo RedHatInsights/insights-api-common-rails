799faa563a136e290bc75985b439d97d8dcaf0a9
Alberto Bellotti <abellott@redhat.com>
2019-03-13 18:23:40 -0400

    Enhancement to move the API to OpenAPI Spec 3.0.0
    - moving from swagger 2 to openapi 3.0.0
    - renaming the swagger:generate to openapi:generate and update accordingly
    
    Updating OpenAPI 3.0 specs now done via:
      $ bundle exec rake openapi:generate

---
+++ lib/manageiq/api/common/open_api/docs/doc_v3.rb
@@ -0,0 +1,52 @@
+module OpenApi
+  class Docs
+    class DocV3
+      attr_reader :content
+
+      def initialize(content)
+        @content = content
+      end
+
+      def version
+        @version ||= Gem::Version.new(content.fetch_path("info", "version"))
+      end
+
+      def parameters
+        @parameters ||= OpenApi::Docs::ComponentCollection.new(self, "components/parameters")
+      end
+
+      def schemas
+        @schemas ||= OpenApi::Docs::ComponentCollection.new(self, "components/schemas")
+      end
+
+      def definitions
+        schemas
+      end
+
+      def example_attributes(key)
+        schemas[key]["properties"].each_with_object({}) do |(col, stuff), hash|
+          hash[col] = stuff["example"] if stuff.key?("example")
+        end
+      end
+
+      def base_path
+        @base_path ||= @content["servers"].first["variables"]["basePath"]["default"]
+      end
+
+      def paths
+        @content["paths"]
+      end
+
+      def routes
+        @routes ||= begin
+          paths.flat_map do |path, hash|
+            hash.collect do |verb, _details|
+              p = File.join(base_path, path).gsub(/{\w*}/, ":id")
+              {:path => p, :verb => verb.upcase}
+            end
+          end
+        end
+      end
+    end
+  end
+end
