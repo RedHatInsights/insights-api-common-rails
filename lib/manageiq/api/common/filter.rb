5153a4d60a0df0f1089bd837b51334ecdcca53ac
Brandon Dunne <brandondunne@hotmail.com>
2019-03-14 13:34:10 -0400

    Add support for filtering result sets
    Add some basic filter operations for strings, integers and datetimes

---
+++ lib/manageiq/api/common/filter.rb
@@ -0,0 +1,152 @@
+class ApplicationController
+  class Filter
+    INTEGER_COMPARISON_KEYWORDS = ["eq", "gt", "gte", "lt", "lte", "nil", "not_nil"].freeze
+    STRING_COMPARISON_KEYWORDS  = ["contains", "eq", "starts_with", "ends_with", "nil", "not_nil"].freeze
+
+    attr_reader :apply, :api_doc_definition
+
+    def initialize(model, raw_filter, api_doc_definition)
+      self.query          = model
+      @raw_filter         = raw_filter
+      @api_doc_definition = api_doc_definition
+    end
+
+    def apply
+      return query if @raw_filter.blank?
+      @raw_filter.each do |k, v|
+        with_attribute_for_key(k) do |attribute|
+          if attribute["type"] == "string"
+            type = determine_string_attribute_type(attribute)
+            send(type, k, v)
+          else
+            errors.add("unsupported attribute type for: #{k}")
+          end
+        end
+      end
+
+      raise ApplicationController::Filter::Error.new(:error_document => errors) unless errors.blank?
+      query
+    end
+
+    private
+
+    attr_accessor :query
+
+    class Error < ArgumentError
+      attr_reader :error_document
+
+      def initialize(attrs)
+        @error_document = attrs[:error_document]
+      end
+    end
+
+    def with_attribute_for_key(key)
+      attribute = api_doc_definition.properties[key.to_s]
+      attribute ? yield(attribute) : errors.add("found unpermitted parameter: #{key}")
+    end
+
+    def determine_string_attribute_type(attribute)
+      return :timestamp if attribute["format"] == "date-time"
+      return :integer if attribute["pattern"] == /^\d+$/
+      :string
+    end
+
+    def errors
+      @errors ||= TopologicalInventory::Api::ErrorDocument.new
+    end
+
+    def string(k, val)
+      if val.kind_of?(ActionController::Parameters)
+        val.each do |comparator, value|
+          add_filter(comparator, STRING_COMPARISON_KEYWORDS, k, value)
+        end
+      else
+        add_filter("eq", STRING_COMPARISON_KEYWORDS, k, val)
+      end
+    end
+
+    def add_filter(requested_comparator, allowed_comparators, key, value)
+      type = with_attribute_for_key(key) { |attribute| determine_string_attribute_type(attribute) }
+
+      if requested_comparator.in?(["nil", "not_nil"])
+        send("comparator_#{requested_comparator}", key, value)
+      elsif requested_comparator.in?(allowed_comparators)
+        value = parse_datetime(value) if type == :datetime
+        return if value.nil?
+        send("comparator_#{requested_comparator}", key, value)
+      else
+        errors.add("unsupported #{type} comparator: #{requested_comparator}")
+      end
+    end
+
+    def timestamp(k, val)
+      if val.kind_of?(ActionController::Parameters)
+        val.each do |comparator, value|
+          add_filter(comparator, INTEGER_COMPARISON_KEYWORDS, k, value)
+        end
+      else
+        add_filter("eq", INTEGER_COMPARISON_KEYWORDS, k, val)
+      end
+    end
+
+    def parse_datetime(value)
+      return value.collect { |i| parse_datetime(i, ) } if value.kind_of?(Array)
+
+      DateTime.parse(value)
+    rescue ArgumentError
+      errors.add("invalid timestamp: #{value}")
+      return nil
+    end
+
+    def integer(k, val)
+      if val.kind_of?(ActionController::Parameters)
+        val.each do |comparator, value|
+          add_filter(comparator, INTEGER_COMPARISON_KEYWORDS, k, value)
+        end
+      else
+        add_filter("eq", INTEGER_COMPARISON_KEYWORDS, k, val)
+      end
+    end
+
+    def comparator_contains(key, value)
+      return value.each { |v| comparator_contains(key, v) } if value.kind_of?(Array)
+      self.query = query.where("#{key} LIKE ?", "%#{value}%")
+    end
+
+    def comparator_starts_with(key, value)
+      self.query = query.where("#{key} LIKE ?", "#{value}%")
+    end
+
+    def comparator_ends_with(key, value)
+      self.query = query.where("#{key} LIKE ?", "%#{value}")
+    end
+
+    def comparator_eq(key, value)
+      self.query = query.where(key => value)
+    end
+
+    def comparator_gt(key, value)
+      self.query = query.where("#{key} > ?", value)
+    end
+
+    def comparator_gte(key, value)
+      self.query = query.where("#{key} >= ?", value)
+    end
+
+    def comparator_lt(key, value)
+      self.query = query.where("#{key} < ?", value)
+    end
+
+    def comparator_lte(key, value)
+      self.query = query.where("#{key} <= ?", value)
+    end
+
+    def comparator_nil(key, _value = nil)
+      self.query = query.where(key => nil)
+    end
+
+    def comparator_not_nil(key, _value = nil)
+      self.query = query.where.not(key => nil)
+    end
+  end
+end
