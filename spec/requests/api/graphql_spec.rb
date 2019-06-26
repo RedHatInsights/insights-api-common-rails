891c81c7d9bbc90f4f27bafafa61d3bb3dd36fb0
Alberto Bellotti <abellott@redhat.com>
2019-06-26 11:20:38 -0400

    Added offset/limit/filter to primary collection queries

--- spec/requests/api/graphql_spec.rb
+++ spec/requests/api/graphql_spec.rb
@@ -7,10 +7,11 @@ RSpec.describe ManageIQ::API::Common::GraphQL, :type => :request do
   let!(:identity1) { Base64.encode64({'identity' => { 'account_number' => ext_tenant1 }}.to_json) }
   let!(:headers)   { { "CONTENT_TYPE" => "application/json", "x-rh-identity" => identity1 } }
 
-  let!(:source_a)   { Source.create!(:tenant_id => tenant1.id, :uid => "12", :name => "source_a", :source_type => source_type) }
-  let!(:source_b)   { Source.create!(:tenant_id => tenant1.id, :uid => "34", :name => "source_b", :source_type => source_type) }
-  let!(:source_c)   { Source.create!(:tenant_id => tenant1.id, :uid => "56", :name => "source_c", :source_type => source_type) }
-  let!(:source_d)   { Source.create!(:tenant_id => tenant1.id, :uid => "78", :name => "source_d", :source_type => source_type) }
+  let!(:source_a1)   { Source.create!(:tenant_id => tenant1.id, :uid => "1", :name => "source_a1", :source_type => source_type) }
+  let!(:source_a2)   { Source.create!(:tenant_id => tenant1.id, :uid => "2", :name => "source_a2", :source_type => source_type) }
+  let!(:source_b1)   { Source.create!(:tenant_id => tenant1.id, :uid => "3", :name => "source_b1", :source_type => source_type) }
+  let!(:source_b2)   { Source.create!(:tenant_id => tenant1.id, :uid => "4", :name => "source_b2", :source_type => source_type) }
+  let!(:source_b3)   { Source.create!(:tenant_id => tenant1.id, :uid => "5", :name => "source_b3", :source_type => source_type) }
 
   let!(:graphql_source_query) { { "query" => "{ sources { id name } }" }.to_json }
 
@@ -18,33 +19,183 @@ RSpec.describe ManageIQ::API::Common::GraphQL, :type => :request do
     before { stub_const("ENV", "BYPASS_TENANCY" => nil) }
 
     it "with no offset or limit returns all sources" do
-      post("/api/v1.0/graphql", :headers => headers, :params => { "query" => "
+      post("/api/v1.0/graphql", :headers => headers, :params => { "query" => '
         {
           sources {
             uid
             name
           }
-        }" }.to_json)
+        }' }.to_json)
 
       expect(response.status).to eq(200)
       expect(response.parsed_body["data"]).to eq(JSON.parse('
         {
           "sources": [
             {
-              "uid": "12",
-              "name": "source_a"
+              "uid": "1",
+              "name": "source_a1"
             },
             {
-              "uid": "34",
-              "name": "source_b"
+              "uid": "2",
+              "name": "source_a2"
             },
             {
-              "uid": "56",
-              "name": "source_c"
+              "uid": "3",
+              "name": "source_b1"
             },
             {
-              "uid": "78",
-              "name": "source_d"
+              "uid": "4",
+              "name": "source_b2"
+            },
+            {
+              "uid": "5",
+              "name": "source_b3"
+            }
+          ]
+        }'))
+    end
+
+    it "honors limit parameter" do
+      post("/api/v1.0/graphql", :headers => headers, :params => { "query" => '
+        {
+          sources(limit: 2) {
+            uid
+            name
+          }
+        }' }.to_json)
+
+      expect(response.status).to eq(200)
+      expect(response.parsed_body["data"]).to eq(JSON.parse('
+        {
+          "sources": [
+            {
+              "uid": "1",
+              "name": "source_a1"
+            },
+            {
+              "uid": "2",
+              "name": "source_a2"
+            }
+          ]
+        }'))
+    end
+
+    it "honors offset parameter" do
+      post("/api/v1.0/graphql", :headers => headers, :params => { "query" => '
+        {
+          sources(offset: 1) {
+            uid
+            name
+          }
+        }' }.to_json)
+
+      expect(response.status).to eq(200)
+      expect(response.parsed_body["data"]).to eq(JSON.parse('
+        {
+          "sources": [
+            {
+              "uid": "2",
+              "name": "source_a2"
+            },
+            {
+              "uid": "3",
+              "name": "source_b1"
+            },
+            {
+              "uid": "4",
+              "name": "source_b2"
+            },
+            {
+              "uid": "5",
+              "name": "source_b3"
+            }
+          ]
+        }'))
+    end
+
+    it "honors offset and limit parameter" do
+      post("/api/v1.0/graphql", :headers => headers, :params => { "query" => '
+        {
+          sources(offset: 1, limit: 2) {
+            uid
+            name
+          }
+        }' }.to_json)
+
+      expect(response.status).to eq(200)
+      expect(response.parsed_body["data"]).to eq(JSON.parse('
+        {
+          "sources": [
+            {
+              "uid": "2",
+              "name": "source_a2"
+            },
+            {
+              "uid": "3",
+              "name": "source_b1"
+            }
+          ]
+        }'))
+    end
+
+    it "honors filter parameter" do
+      post("/api/v1.0/graphql", :headers => headers, :params => { "query" => '
+        {
+          sources(filter: { name: { starts_with: "source_b"}}) {
+            name
+          }
+        }' }.to_json)
+
+      expect(response.status).to eq(200)
+      expect(response.parsed_body["data"]).to eq(JSON.parse('
+        {
+          "sources": [
+            {
+              "name": "source_b1"
+            },
+            {
+              "name": "source_b2"
+            },
+            {
+              "name": "source_b3"
+            }
+          ]
+        }'))
+    end
+
+    it "honors filter and limit parameter" do
+      post("/api/v1.0/graphql", :headers => headers, :params => { "query" => '
+        {
+          sources(filter: { name: { ends_with: "2"}}, limit: 1) {
+            name
+          }
+        }' }.to_json)
+
+      expect(response.status).to eq(200)
+      expect(response.parsed_body["data"]).to eq(JSON.parse('
+        {
+          "sources": [
+            {
+              "name": "source_a2"
+            }
+          ]
+        }'))
+    end
+
+    it "honors filter with offset and limit parameter" do
+      post("/api/v1.0/graphql", :headers => headers, :params => { "query" => '
+        {
+          sources(filter: { name: { starts_with: "source_b"}}, offset: 1, limit: 1) {
+            name
+          }
+        }' }.to_json)
+
+      expect(response.status).to eq(200)
+      expect(response.parsed_body["data"]).to eq(JSON.parse('
+        {
+          "sources": [
+            {
+              "name": "source_b2"
             }
           ]
         }'))
