1c8d67175f9244713bd241233f573efea9b7946e
Brandon Dunne <brandondunne@hotmail.com>
2019-08-26 16:13:36 -0400

    Allow numeric or non-numeric IDs to be parsed
    Find will fail later and return the correct 404 response

--- spec/lib/manageiq/api/common/application_controller_mixins/request_path_spec.rb
+++ spec/lib/manageiq/api/common/application_controller_mixins/request_path_spec.rb
@@ -27,29 +27,29 @@ describe ManageIQ::API::Common::ApplicationControllerMixins::RequestPath do
       expect(test_class.new("/v1.0/primary/1/").subcollection?).to      eq(false)
       expect(test_class.new("/v1.0/primary/1/sub").subcollection?).to   eq(true)
       expect(test_class.new("/v1.0/primary/1/sub/").subcollection?).to  eq(true)
+      expect(test_class.new("/v1.0/primary/a_b/sub").subcollection?).to eq(true)
     end
 
     it "invalid paths" do
-      expect(test_class.new("/v1.0/primary/abc/sub").subcollection?).to eq(false)
-      expect(test_class.new("/primary/abc/sub").subcollection?).to      eq(false)
+      expect(test_class.new("/primary/abc/sub").subcollection?).to eq(false)
     end
   end
 
   context "#request_path_parts" do
     it "valid paths" do
-      expect(test_class.new("/aaa/bbb/v1.0/primary").request_path_parts).to eq("full_version_string" => "v1.0", "primary_collection_id" => nil, "primary_collection_name" => "primary", "subcollection_name" => nil)
-      expect(test_class.new("/aaa/v1.0/primary").request_path_parts).to     eq("full_version_string" => "v1.0", "primary_collection_id" => nil, "primary_collection_name" => "primary", "subcollection_name" => nil)
-      expect(test_class.new("/v1.0/primary").request_path_parts).to         eq("full_version_string" => "v1.0", "primary_collection_id" => nil, "primary_collection_name" => "primary", "subcollection_name" => nil)
-      expect(test_class.new("/v1.0/primary/").request_path_parts).to        eq("full_version_string" => "v1.0", "primary_collection_id" => nil, "primary_collection_name" => "primary", "subcollection_name" => nil)
-      expect(test_class.new("/v1.0/primary/1").request_path_parts).to       eq("full_version_string" => "v1.0", "primary_collection_id" => "1", "primary_collection_name" => "primary", "subcollection_name" => nil)
-      expect(test_class.new("/v1.0/primary/1/").request_path_parts).to      eq("full_version_string" => "v1.0", "primary_collection_id" => "1", "primary_collection_name" => "primary", "subcollection_name" => nil)
-      expect(test_class.new("/v1.0/primary/1/sub").request_path_parts).to   eq("full_version_string" => "v1.0", "primary_collection_id" => "1", "primary_collection_name" => "primary", "subcollection_name" => "sub")
-      expect(test_class.new("/v1.0/primary/1/sub/").request_path_parts).to  eq("full_version_string" => "v1.0", "primary_collection_id" => "1", "primary_collection_name" => "primary", "subcollection_name" => "sub")
+      expect(test_class.new("/aaa/bbb/v1.0/primary").request_path_parts).to eq("full_version_string" => "v1.0", "primary_collection_id" => nil,   "primary_collection_name" => "primary", "subcollection_name" => nil)
+      expect(test_class.new("/aaa/v1.0/primary").request_path_parts).to     eq("full_version_string" => "v1.0", "primary_collection_id" => nil,   "primary_collection_name" => "primary", "subcollection_name" => nil)
+      expect(test_class.new("/v1.0/primary").request_path_parts).to         eq("full_version_string" => "v1.0", "primary_collection_id" => nil,   "primary_collection_name" => "primary", "subcollection_name" => nil)
+      expect(test_class.new("/v1.0/primary/").request_path_parts).to        eq("full_version_string" => "v1.0", "primary_collection_id" => nil,   "primary_collection_name" => "primary", "subcollection_name" => nil)
+      expect(test_class.new("/v1.0/primary/1").request_path_parts).to       eq("full_version_string" => "v1.0", "primary_collection_id" => "1",   "primary_collection_name" => "primary", "subcollection_name" => nil)
+      expect(test_class.new("/v1.0/primary/1/").request_path_parts).to      eq("full_version_string" => "v1.0", "primary_collection_id" => "1",   "primary_collection_name" => "primary", "subcollection_name" => nil)
+      expect(test_class.new("/v1.0/primary/1/sub").request_path_parts).to   eq("full_version_string" => "v1.0", "primary_collection_id" => "1",   "primary_collection_name" => "primary", "subcollection_name" => "sub")
+      expect(test_class.new("/v1.0/primary/1/sub/").request_path_parts).to  eq("full_version_string" => "v1.0", "primary_collection_id" => "1",   "primary_collection_name" => "primary", "subcollection_name" => "sub")
+      expect(test_class.new("/v1.0/primary/a_b/sub").request_path_parts).to eq("full_version_string" => "v1.0", "primary_collection_id" => "a_b", "primary_collection_name" => "primary", "subcollection_name" => "sub")
     end
 
     it "invalid paths" do
-      expect(test_class.new("/v1.0/primary/abc/sub").request_path_parts).to eq("full_version_string" => "v1.0", "primary_collection_id" => nil, "primary_collection_name" => "primary", "subcollection_name" => nil)
-      expect(test_class.new("/primary/1/sub").request_path_parts).to        eq({})
+      expect(test_class.new("/primary/1/sub").request_path_parts).to eq({})
     end
   end
 end
