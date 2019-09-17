153127cc9e3f97460cab97c8796840561dfcdeaf
Brandon Dunne <brandondunne@hotmail.com>
2019-09-17 16:57:15 -0400

    Extract common #build_paths

--- lib/manageiq/api/common/open_api/generator.rb
+++ lib/manageiq/api/common/open_api/generator.rb
@@ -391,6 +391,49 @@ module ManageIQ
           def generator_read_only_definitions
             @generator_read_only_definitions ||= [].to_set.freeze
           end
+
+          def build_paths
+            applicable_rails_routes.each_with_object({}) do |route, expected_paths|
+              without_format = route.path.split("(.:format)").first
+              sub_path = without_format.split(base_path).last.sub(/:[_a-z]*id/, "{id}")
+              klass_name = route.controller.split("/").last.camelize.singularize
+              verb = route.verb.downcase
+              primary_collection = sub_path.split("/")[1].camelize.singularize
+
+              expected_paths[sub_path] ||= {}
+              expected_paths[sub_path][verb] =
+                case route.action
+                  when "index"   then openapi_list_description(klass_name, primary_collection)
+                  when "show"    then openapi_show_description(klass_name)
+                  when "destroy" then openapi_destroy_description(klass_name)
+                  when "create"  then openapi_create_description(klass_name)
+                  when "update"  then openapi_update_description(klass_name, verb)
+                  else                handle_custom_route_action(route.action.camelize, verb, primary_collection)
+                end
+
+              unless expected_paths[sub_path][verb]
+                # If it's not generic action but a custom method like e.g. `post "order", :to => "service_plans#order"`, we will
+                # try to take existing schema, because the description, summary, etc. are likely to be custom.
+                expected_paths[sub_path][verb] =
+                  case verb
+                  when "post"
+                    if sub_path == "/graphql" && route.action == "query"
+                      schemas["GraphQLResponse"] = ::ManageIQ::API::Common::GraphQL.openapi_graphql_response
+                      ::ManageIQ::API::Common::GraphQL.openapi_graphql_description
+                    else
+                      openapi_contents.dig("paths", sub_path, verb) || openapi_create_description(klass_name)
+                    end
+                  when "get"
+                    openapi_contents.dig("paths", sub_path, verb) || openapi_show_description(klass_name)
+                  else
+                    openapi_contents.dig("paths", sub_path, verb)
+                  end
+              end
+            end
+          end
+
+          def handle_custom_route_action(_route_action, _verb, _primary_collection)
+          end
         end
       end
     end
