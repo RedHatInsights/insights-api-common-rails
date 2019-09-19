e40930536014b3ce2cfa53643ce59b75133fe2fb
Brandon Dunne <brandondunne@hotmail.com>
2019-09-19 11:23:48 -0400

    Move request path id validation to a before_action

--- lib/manageiq/api/common/application_controller_mixins/request_path.rb
+++ lib/manageiq/api/common/application_controller_mixins/request_path.rb
@@ -7,6 +7,8 @@ module ManageIQ
           end
 
           def self.included(other)
+            other.before_action(:validate_primary_collection_id)
+
             other.rescue_from(ManageIQ::API::Common::ApplicationControllerMixins::RequestPath::RequestPathError) do |exception|
               error_document = ManageIQ::API::Common::ErrorDocument.new.add(400, exception.message)
               render :json => error_document.to_h, :status => error_document.status
@@ -18,15 +20,19 @@ module ManageIQ
           end
 
           def request_path_parts
-            @request_path_parts ||=
-              (request_path.match(/\/(?<full_version_string>v\d+.\d+)\/(?<primary_collection_name>\w+)(\/(?<primary_collection_id>[^\/]+)(\/(?<subcollection_name>\w+))?)?/)&.named_captures || {}).tap do |named_captures|
-                id = named_captures["primary_collection_id"].presence
-                raise RequestPathError, "ID is invalid" if id && !id.match(/^\d+$/)
-              end
+            @request_path_parts ||= request_path.match(/\/(?<full_version_string>v\d+.\d+)\/(?<primary_collection_name>\w+)(\/(?<primary_collection_id>[^\/]+)(\/(?<subcollection_name>\w+))?)?/)&.named_captures || {}
           end
 
           def subcollection?
             !!(request_path_parts["subcollection_name"] && request_path_parts["primary_collection_id"] && request_path_parts["primary_collection_name"])
+          end
+
+          private
+
+          def validate_primary_collection_id
+            id = request_path_parts["primary_collection_id"]
+            return if id.blank?
+            raise RequestPathError, "ID is invalid" if !id.match(/^\d+$/)
           end
         end
       end
