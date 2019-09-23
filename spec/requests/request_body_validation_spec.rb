97324177580389a1c2010cc270c80bf4309ab14c
Erik Clarizio <eclarizio@gmail.com>
2019-09-23 13:24:53 -0700

    Remove unnecessary '.to_json' calls in request_body_validation_spec

--- spec/requests/request_body_validation_spec.rb
+++ spec/requests/request_body_validation_spec.rb
@@ -4,63 +4,67 @@ RSpec.describe "ManageIQ::API::Common::ApplicationController Body", :type => :re
   before { stub_const("ENV", "BYPASS_TENANCY" => true) }
   let(:default_params) { { "authtype" => "openshift" } }
 
-  it "invalid body" do
-    post("/api/v1.0/authentications", :headers => {"CONTENT_TYPE" => "application/text"}, :params => "{")
+  context "when there is an invalid body" do
+    let(:default_as) { :text }
 
-    expect(response.status).to eq(400)
+    it "returns a 400" do
+      post("/api/v1.0/authentications", :headers => {"CONTENT_TYPE" => "application/text"}, :params => "{")
+
+      expect(response.status).to eq(400)
+    end
   end
 
   it "unpermitted key" do
-    post("/api/v1.0/authentications", :headers => headers, :params => default_params.merge("garbage" => "abc").to_json)
+    post("/api/v1.0/authentications", :headers => headers, :params => default_params.merge("garbage" => "abc"))
 
     expect(response.status).to eq(400)
     expect(response.parsed_body).to eq("errors" => [{"detail" => "properties garbage are not defined in #/components/schemas/Authentication", "status" => 400}])
   end
 
   it "permitted key, good value" do
-    post("/api/v1.0/authentications", :headers => headers, :params => default_params.merge("username" => "abc").to_json)
+    post("/api/v1.0/authentications", :headers => headers, :params => default_params.merge("username" => "abc"))
 
     expect(response.status).to eq(200)
     expect(response.parsed_body).to eq("OK")
   end
 
   it "permitted key, bad value" do
-    post("/api/v1.0/authentications", :headers => headers, :params => default_params.merge("username" => 1).to_json)
+    post("/api/v1.0/authentications", :headers => headers, :params => default_params.merge("username" => 1))
 
     expect(response.status).to eq(400)
     expect(response.parsed_body).to eq("errors" => [{"detail" => "1 class is Integer but it's not valid string in #/components/schemas/Authentication/properties/username", "status" => 400}])
   end
 
   it "permitted key, array" do
-    post("/api/v1.0/authentications", :headers => headers, :params => default_params.merge("array" => [1]).to_json)
+    post("/api/v1.0/authentications", :headers => headers, :params => default_params.merge("array" => [1]))
 
     expect(response.status).to eq(200)
     expect(response.parsed_body).to eq("OK")
   end
 
   it "permitted key, hash" do
-    post("/api/v1.0/authentications", :headers => headers, :params => default_params.merge("hash" => {"a" => 1}).to_json)
+    post("/api/v1.0/authentications", :headers => headers, :params => default_params.merge("hash" => {"a" => 1}))
 
     expect(response.status).to eq(200)
     expect(response.parsed_body).to eq("OK")
   end
 
   it "permitted key, hash nested" do
-    post("/api/v1.0/authentications", :headers => headers, :params => default_params.merge("hash" => {"a" => {1 => {}}}).to_json)
+    post("/api/v1.0/authentications", :headers => headers, :params => default_params.merge("hash" => {"a" => {1 => {}}}))
 
     expect(response.status).to eq(200)
     expect(response.parsed_body).to eq("OK")
   end
 
   it "required property is missing" do
-    post("/api/v1.0/authentications", :headers => headers, :params => {"username" => "abc"}.to_json)
+    post("/api/v1.0/authentications", :headers => headers, :params => {"username" => "abc"})
 
     expect(response.status).to eq(400)
     expect(response.parsed_body).to eq("errors" => [{"detail" => "required parameters authtype not exist in #/components/schemas/Authentication", "status" => 400}])
   end
 
   it "empty body" do
-    post("/api/v1.0/authentications", :headers => headers, :params => "")
+    post("/api/v1.0/authentications", :headers => headers)
 
     expect(response.status).to eq(400)
     expect(response.parsed_body).to eq("errors" => [{"detail" => "required parameters authtype not exist in #/components/schemas/Authentication", "status" => 400}])
