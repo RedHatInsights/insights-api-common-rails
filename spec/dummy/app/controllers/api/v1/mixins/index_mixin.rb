af36f1550ad88fea6825e042ff964b3868396ef6
Alberto Bellotti <abellott@redhat.com>
2019-10-11 16:59:00 -0400

    Rubocop fixes

--- spec/dummy/app/controllers/api/v1/mixins/index_mixin.rb
+++ spec/dummy/app/controllers/api/v1/mixins/index_mixin.rb
@@ -4,11 +4,11 @@ module Api
       module IndexMixin
         def index
           raise_unless_primary_instance_exists
-          render json: ManageIQ::API::Common::PaginatedResponse.new(
-            base_query: scoped(filtered.where(params_for_list)),
-            request: request,
-            limit: pagination_limit,
-            offset: pagination_offset
+          render :json => ManageIQ::API::Common::PaginatedResponse.new(
+            :base_query => scoped(filtered.where(params_for_list)),
+            :request    => request,
+            :limit      => pagination_limit,
+            :offset     => pagination_offset
           ).response
         end
 
