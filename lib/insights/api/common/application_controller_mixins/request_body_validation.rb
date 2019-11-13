1cf5a0d5948fc7e580f987b7a140c552a7d8df38
Madhu Kanoor <mkanoor@redhat.com>
2019-11-13 16:14:27 -0500

    Applied PR feedback

--- lib/insights/api/common/application_controller_mixins/request_body_validation.rb
+++ lib/insights/api/common/application_controller_mixins/request_body_validation.rb
@@ -18,9 +18,9 @@ module Insights
 
           def body_params
             @body_params ||= begin
-              hash = params.permit!.to_h
-              request.path_parameters.keys.each { |key| hash.delete(key) }
-              ActionController::Parameters.new(hash).permit!
+              ActionController::Parameters.new(
+                params.permit!.to_h.except(*request.path_parameters.keys)
+              ).permit!
             end
           end
 
