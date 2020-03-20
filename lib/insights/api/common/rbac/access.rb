3a57a388ab96ee2d53e6672ddc7518eb562e3930
Madhu Kanoor <mkanoor@redhat.com>
2020-03-20 09:46:31 -0400

    Applied feedback
    Since we are going to be releasing a new gem deleted the old
    functions since they are not going to be used anymore

--- lib/insights/api/common/rbac/access.rb
+++ lib/insights/api/common/rbac/access.rb
@@ -5,49 +5,32 @@ module Insights
         class Access
           attr_reader :acl
           DEFAULT_LIMIT = 500
-          def initialize(resource, verb, app_name = ENV["APP_NAME"])
-            @resource = resource
-            @verb     = verb
+          def initialize(app_name = ENV["APP_NAME"])
             @app_name = app_name
           end
 
           def process
-            regexp = create_regexp(@app_name, @resource, @verb) if @app_name && @resource && @verb
             Service.call(RBACApiClient::AccessApi) do |api|
-              @acl ||= Service.paginate(api, :get_principal_access, {:limit => DEFAULT_LIMIT}, @app_name).select do |item|
-                regexp ? regexp.match?(item.permission) : true
-              end
+              @acl ||= Service.paginate(api, :get_principal_access, {:limit => DEFAULT_LIMIT}, @app_name).to_a
             end
             self
           end
 
-          def accessible?
-            @acl.any?
-          end
-
-          def is_accessible?(app_name, resource, verb)
+          def accessible?(resource, verb, app_name = ENV['APP_NAME'])
             regexp = create_regexp(app_name, resource, verb)
             @acl.any? { |item| regexp.match?(item.permission) }
           end
 
-          def admin_scope?(app_name, resource, verb)
+          def admin_scope?(resource, verb, app_name = ENV['APP_NAME'])
             scope?(app_name, resource, verb, "admin")
           end
 
-          def group_scope?(app_name, resource, verb)
+          def group_scope?(resource, verb, app_name = ENV['APP_NAME'])
             scope?(app_name, resource, verb, "group")
           end
 
-          def user_scope?(app_name, resource, verb)
+          def user_scope?(resource, verb, app_name = ENV['APP_NAME'])
             scope?(app_name, resource, verb, "user")
-          end
-
-          def id_list
-            ids.include?('*') ? [] : ids
-          end
-
-          def owner_scoped?
-            ids.include?('*') ? false : owner_scope_filter?
           end
 
           def self.enabled?
@@ -55,27 +38,6 @@ module Insights
           end
 
           private
-
-          def ids
-            @ids ||= @acl.each_with_object([]) do |item, ids|
-              item.resource_definitions.each do |rd|
-                next unless rd.attribute_filter.key == 'id'
-                next unless rd.attribute_filter.operation == 'equal'
-
-                ids << rd.attribute_filter.value
-              end
-            end
-          end
-
-          def owner_scope_filter?
-            @acl.any? do |item|
-              item.resource_definitions.any? do |rd|
-                rd.attribute_filter.key == 'owner' &&
-                  rd.attribute_filter.operation == 'equal' &&
-                  rd.attribute_filter.value == '{{username}}'
-              end
-            end
-          end
 
           def scope?(app_name, resource, verb, scope)
             regexp = create_regexp(app_name, resource, verb)
--- spec/lib/insights/api/common/rbac/access_spec.rb
+++ spec/lib/insights/api/common/rbac/access_spec.rb
@@ -2,8 +2,8 @@ describe Insights::API::Common::RBAC::Access do
   include_context "rbac_objects"
 
   let(:verb) { "read" }
-  let(:rbac_access) { described_class.new(resource, verb) }
-  let(:rbac_all_acls) { described_class.new(nil, nil) }
+  let(:rbac_access) { described_class.new }
+  let(:access_obj) { rbac_access.process }
   let(:api_instance) { double }
   let(:rs_class) { class_double("Insights::API::Common::RBAC::Service").as_stubbed_const(:transfer_nested_constants => true) }
   let(:opts) { { :limit => described_class::DEFAULT_LIMIT } }
@@ -11,22 +11,6 @@ describe Insights::API::Common::RBAC::Access do
   before do
     stub_const("ENV", "APP_NAME" => app_name)
     allow(rs_class).to receive(:call).with(RBACApiClient::AccessApi).and_yield(api_instance)
-  end
-
-  it "fetches the array of ids" do
-    allow(Insights::API::Common::RBAC::Service).to receive(:paginate).with(api_instance, :get_principal_access, opts, app_name).and_return([access1])
-    svc_obj = rbac_access.process
-    expect(svc_obj.acl.count).to eq(1)
-    expect(svc_obj.accessible?).to be_truthy
-    expect(svc_obj.id_list).to match_array([resource_id1])
-  end
-
-  it "* in id gives access to all instances" do
-    allow(Insights::API::Common::RBAC::Service).to receive(:paginate).with(api_instance, :get_principal_access, opts, app_name).and_return([admin_access])
-    svc_obj = rbac_access.process
-    expect(svc_obj.acl.count).to eq(1)
-    expect(svc_obj.accessible?).to be_truthy
-    expect(svc_obj.id_list).to match_array([])
   end
 
   it "rbac is enabled by default" do
@@ -40,24 +24,22 @@ describe Insights::API::Common::RBAC::Access do
 
   it "checks admin scope" do
     allow(Insights::API::Common::RBAC::Service).to receive(:paginate).with(api_instance, :get_principal_access, opts, app_name).and_return([admin_scope])
-    svc_obj = rbac_all_acls.process
-    expect(svc_obj.is_accessible?(app_name, resource, 'read')).to be_truthy
-    expect(svc_obj.admin_scope?(app_name, resource, 'read')).to be_truthy
-    expect(svc_obj.user_scope?(app_name, resource, 'read')).to be_falsey
-    expect(svc_obj.group_scope?(app_name, resource, 'read')).to be_falsey
+    expect(access_obj.accessible?(resource, 'read')).to be_truthy
+    expect(access_obj.admin_scope?(resource, 'read')).to be_truthy
+    expect(access_obj.user_scope?(resource, 'read')).to be_falsey
+    expect(access_obj.group_scope?(resource, 'read')).to be_falsey
   end
 
   it "checks user scope" do
     allow(Insights::API::Common::RBAC::Service).to receive(:paginate).with(api_instance, :get_principal_access, opts, app_name).and_return([user_scope])
-    svc_obj = rbac_all_acls.process
-    expect(svc_obj.is_accessible?(app_name, resource, 'read')).to be_truthy
-    expect(svc_obj.user_scope?(app_name, resource, 'read')).to be_truthy
+
+    expect(access_obj.accessible?(resource, 'read', app_name)).to be_truthy
+    expect(access_obj.user_scope?(resource, 'read', app_name)).to be_truthy
   end
 
   it "checks group scope" do
     allow(Insights::API::Common::RBAC::Service).to receive(:paginate).with(api_instance, :get_principal_access, opts, app_name).and_return([group_scope])
-    svc_obj = rbac_all_acls.process
-    expect(svc_obj.is_accessible?(app_name, resource, 'read')).to be_truthy
-    expect(svc_obj.group_scope?(app_name, resource, 'read')).to be_truthy
+    expect(access_obj.accessible?(resource, 'read', app_name)).to be_truthy
+    expect(access_obj.group_scope?(resource, 'read')).to be_truthy
   end
 end
