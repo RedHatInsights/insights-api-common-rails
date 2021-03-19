088b5c0d8f142c98600c4b9092eca013eaa9c60c
Libor Pichler <lpichler@redhat.com>
2021-03-19 19:49:03 +0100

    Add spec to check enabled GraphqL introspection in generator_spec

--- spec/lib/insights/api/common/graphql/generator_spec.rb
+++ spec/lib/insights/api/common/graphql/generator_spec.rb
@@ -71,59 +71,68 @@ RSpec.describe Insights::API::Common::GraphQL::Generator do
     let(:graphql_schema_pluggable_directory) { Pathname.new(graphql_schema_pluggable) }
     let(:schema_path)                        { graphql_schema_pluggable_directory.join("schema.erb") }
 
-    before do
-      ::Insights::API::Common::GraphQL::Api.send(:remove_const, "V2x0") if ::Insights::API::Common::GraphQL::Api.const_defined?("V2x0", false)
+    %i[enabled_introspection disabled_introspection].each do |introspection|
+      context "Introspection settings: #{introspection}" do
+        before do
+          ::Insights::API::Common::GraphQL::Api.send(:remove_const, "V2x0") if ::Insights::API::Common::GraphQL::Api.const_defined?("V2x0", false)
 
-      allow(described_class).to receive(:root_dir).and_return(root_dir)
-
-      FileUtils.mkpath(graphql_schema_pluggable_directory)
-      File.write(schema_path, graphql_schema)
-    end
-
-    after do
-      ::Insights::API::Common::GraphQL::Api.send(:remove_const, "V2x0")
-
-      FileUtils.rm_r(schema_path)
-    end
-
-    it "support base_query" do
-      graphql_request = double
-      allow(graphql_request).to receive(:original_url).and_return(graphql_endpoint_v2)
-
-      schema_overlay = {
-        "^source_types$" => {
-          "base_query" => lambda do |model_class, _args, _ctx|
-            model_class.where(:vendor => "redhat")
+          if introspection == :disabled_introspection
+            allow(described_class).to receive(:root_dir).and_return(root_dir)
+            FileUtils.mkpath(graphql_schema_pluggable_directory)
+            File.write(schema_path, graphql_schema)
           end
-        }
-      }
+        end
 
-      graphql_query = '
-        {
-          source_types(sort_by: { name: "asc" }) {
-            name
-            vendor
-          }
-        }
-      '
+        after do
+          ::Insights::API::Common::GraphQL::Api.send(:remove_const, "V2x0")
 
-      graphql_api_schema = described_class.init_schema_v2(graphql_request, schema_overlay)
-      expect(graphql_api_schema.disable_introspection_entry_points).to be_truthy
+          FileUtils.rm_r(schema_path) if introspection == :disabled_introspection
+        end
 
-      result = graphql_api_schema.execute(graphql_query, :variables => {})
-      expect(result["data"]).to eq(JSON.parse('
-        {
-          "source_types": [
-            {
-              "name": "openshift_test",
-              "vendor": "redhat"
-            },
-            {
-              "name": "rhev_test",
-              "vendor": "redhat"
+        it "support base_query" do
+          graphql_request = double
+          allow(graphql_request).to receive(:original_url).and_return(graphql_endpoint_v2)
+
+          schema_overlay = {
+            "^source_types$" => {
+              "base_query" => lambda do |model_class, _args, _ctx|
+                model_class.where(:vendor => "redhat")
+              end
             }
-          ]
-        }'))
+          }
+
+          graphql_query = '
+            {
+              source_types(sort_by: { name: "asc" }) {
+                name
+                vendor
+              }
+            }
+          '
+
+          graphql_api_schema = described_class.init_schema_v2(graphql_request, schema_overlay)
+          if introspection == :disabled_introspection
+            expect(graphql_api_schema.disable_introspection_entry_points).to be_truthy
+          else
+            expect(graphql_api_schema.disable_introspection_entry_points).to be_falsey
+          end
+
+          result = graphql_api_schema.execute(graphql_query, :variables => {})
+          expect(result["data"]).to eq(JSON.parse('
+            {
+              "source_types": [
+                {
+                  "name": "openshift_test",
+                  "vendor": "redhat"
+                },
+                {
+                  "name": "rhev_test",
+                  "vendor": "redhat"
+                }
+              ]
+            }'))
+        end
+      end
     end
   end
 end
