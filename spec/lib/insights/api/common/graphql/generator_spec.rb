5b87eaa975dc3067ebf86e30c94132c0f6dcac3f
Libor Pichler <lpichler@redhat.com>
2021-03-19 12:32:15 +0100

    Allow to load GraphQL erb templates from root directory in generator

--- spec/lib/insights/api/common/graphql/generator_spec.rb
+++ spec/lib/insights/api/common/graphql/generator_spec.rb
@@ -53,8 +53,38 @@ RSpec.describe Insights::API::Common::GraphQL::Generator do
   end
 
   context "schema overlays v2" do
-    before { ::Insights::API::Common::GraphQL::Api.send(:remove_const, "V2x0") if ::Insights::API::Common::GraphQL::Api.const_defined?("V2x0", false) }
-    after  { ::Insights::API::Common::GraphQL::Api.send(:remove_const, "V2x0") }
+    let(:graphql_schema) do
+      <<~RUBY
+        Schema = ::GraphQL::Schema.define do
+          use ::GraphQL::Batch
+          enable_preloading
+          disable_introspection_entry_points
+
+          query QueryType
+        end
+      RUBY
+    end
+
+    let(:application_name)                   { Rails.application.class.parent.name.underscore }
+    let(:root_dir)                           { Dir.mktmpdir("root_dir") }
+    let(:graphql_schema_pluggable)           { "#{root_dir}/lib/#{application_name}/api/graphql/templates" }
+    let(:graphql_schema_pluggable_directory) { Pathname.new(graphql_schema_pluggable) }
+    let(:schema_path)                        { graphql_schema_pluggable_directory.join("schema.erb") }
+
+    before do
+      ::Insights::API::Common::GraphQL::Api.send(:remove_const, "V2x0") if ::Insights::API::Common::GraphQL::Api.const_defined?("V2x0", false)
+
+      allow(described_class).to receive(:root_dir).and_return(root_dir)
+
+      FileUtils.mkpath(graphql_schema_pluggable_directory)
+      File.write(schema_path, graphql_schema)
+    end
+
+    after do
+      ::Insights::API::Common::GraphQL::Api.send(:remove_const, "V2x0")
+
+      FileUtils.rm_r(schema_path)
+    end
 
     it "support base_query" do
       graphql_request = double
@@ -78,8 +108,9 @@ RSpec.describe Insights::API::Common::GraphQL::Generator do
       '
 
       graphql_api_schema = described_class.init_schema_v2(graphql_request, schema_overlay)
-      result = graphql_api_schema.execute(graphql_query, :variables => {})
+      expect(graphql_api_schema.disable_introspection_entry_points).to be_truthy
 
+      result = graphql_api_schema.execute(graphql_query, :variables => {})
       expect(result["data"]).to eq(JSON.parse('
         {
           "source_types": [
