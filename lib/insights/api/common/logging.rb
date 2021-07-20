ManageIQ/manageiq-api-common

Use insights logger gem

lib/insights/api/common/logging.rb
@@ -2,14 +2,27 @@ module Insights
   module API
     module Common
       class Logging
-        def self.activate(config)
-          require 'manageiq/loggers'
-          config.logger = if Rails.env.production?
-                            config.colorize_logging = false
-                            ManageIQ::Loggers::CloudWatch.new
-                          else
-                            ManageIQ::Loggers::Base.new(Rails.root.join("log", "#{Rails.env}.log"))
-                          end
+        def self.logger_class
+          if ENV['LOG_HANDLER'] == "haberdasher"
+            "Insights::Loggers::StdErrorLogger"
+          else
+            "ManageIQ::Loggers::CloudWatch"
+          end
+        end
+
+        def self.activate(config, app_name = nil)
+          require 'insights/loggers'
+          log_params = {}
+          klass_for_logger = if Rails.env.production?
+                               config.colorize_logging = false
+                               log_params[:app_name] = app_name if app_name
+                               logger_class
+                             else
+                               log_params = {:log_path => Rails.root.join("log", "#{Rails.env}.log")}
+                               "ManageIQ::Loggers::Base"
+                             end
+
+          config.logger = Insights::Loggers::Factory.create_logger(klass_for_logger, log_params)
         end
       end
     end

spec/lib/insights/api/common/logging_spec.rb
@@ -1,0 +1,52 @@
+require 'cloudwatchlogger'
+
+describe Insights::API::Common::Metrics do
+  let(:rails_config) { Rails.application.config }
+
+  subject { Insights::API::Common::Logging.activate(rails_config) }
+
+  context "development environment" do
+    before do
+      allow(Rails).to receive(:env).and_return(ActiveSupport::StringInquirer.new("development"))
+    end
+
+    it "return proper logger class" do
+      expect(subject.class).to eq(ManageIQ::Loggers::Base)
+    end
+
+    context "haberdasher is set as log_handler" do
+      before do
+        ENV['LOG_HANDLER'] == "haberdasher"
+      end
+
+      it "return proper logger class" do
+        expect(subject.class).to eq(ManageIQ::Loggers::Base)
+      end
+    end
+  end
+
+  context "production environment" do
+    before do
+      ENV["CW_AWS_ACCESS_KEY_ID"] = "test"
+      ENV["CW_AWS_SECRET_ACCESS_KEY"] = "test"
+      ENV["CLOUD_WATCH_LOG_GROUP"] = "test"
+      ENV["HOSTNAME"] = "test"
+      allow(CloudWatchLogger::Client::AWS_SDK::DeliveryThreadManager).to receive(:new)
+      allow(Rails).to receive(:env).and_return(ActiveSupport::StringInquirer.new("production"))
+    end
+
+    it "return proper logger class" do
+      expect(subject.class).to eq(ManageIQ::Loggers::CloudWatch)
+    end
+
+    context "haberdasher is set as log_handler" do
+      before do
+        ENV['LOG_HANDLER'] = "haberdasher"
+      end
+
+      it "return proper logger class" do
+        expect(subject.class).to eq(Insights::Loggers::StdErrorLogger)
+      end
+    end
+  end
+end

RedHatInsights/insights-api-common-rails

lib/insights/api/common/logging.rb
@@ -2,14 +2,27 @@ module Insights
   module API
     module Common
       class Logging
-        def self.activate(config)
-          require 'manageiq/loggers'
-          config.logger = if Rails.env.production?
-                            config.colorize_logging = false
-                            ManageIQ::Loggers::CloudWatch.new
-                          else
-                            ManageIQ::Loggers::Base.new(Rails.root.join("log", "#{Rails.env}.log"))
-                          end
+        def self.logger_class
+          if ENV['LOG_HANDLER'] == "haberdasher"
+            "Insights::Loggers::StdErrorLogger"
+          else
+            "Insights::Loggers::CloudWatch"
+          end
+        end
+
+        def self.activate(config, app_name = nil)
+          require 'insights/loggers'
+          log_params = {}
+          klass_for_logger = if Rails.env.production?
+                               config.colorize_logging = false
+                               log_params[:app_name] = app_name if app_name
+                               logger_class
+                             else
+                               log_params = {:log_path => Rails.root.join("log", "#{Rails.env}.log")}
+                               "ManageIQ::Loggers::Base"
+                             end
+
+          config.logger = Insights::Loggers::Factory.create_logger(klass_for_logger, log_params)
         end
       end
     end

spec/lib/insights/api/common/logging_spec.rb
@@ -1,0 +1,52 @@
+require 'cloudwatchlogger'
+
+describe Insights::API::Common::Metrics do
+  let(:rails_config) { Rails.application.config }
+
+  subject { Insights::API::Common::Logging.activate(rails_config) }
+
+  context "development environment" do
+    before do
+      allow(Rails).to receive(:env).and_return(ActiveSupport::StringInquirer.new("development"))
+    end
+
+    it "return proper logger class" do
+      expect(subject.class).to eq(ManageIQ::Loggers::Base)
+    end
+
+    context "haberdasher is set as log_handler" do
+      before do
+        ENV['LOG_HANDLER'] == "haberdasher"
+      end
+
+      it "return proper logger class" do
+        expect(subject.class).to eq(ManageIQ::Loggers::Base)
+      end
+    end
+  end
+
+  context "production environment" do
+    before do
+      ENV["CW_AWS_ACCESS_KEY_ID"] = "test"
+      ENV["CW_AWS_SECRET_ACCESS_KEY"] = "test"
+      ENV["CLOUD_WATCH_LOG_GROUP"] = "test"
+      ENV["HOSTNAME"] = "test"
+      allow(CloudWatchLogger::Client::AWS_SDK::DeliveryThreadManager).to receive(:new)
+      allow(Rails).to receive(:env).and_return(ActiveSupport::StringInquirer.new("production"))
+    end
+
+    it "return proper logger class" do
+      expect(subject.class).to eq(Insights::Loggers::CloudWatch)
+    end
+
+    context "haberdasher is set as log_handler" do
+      before do
+        ENV['LOG_HANDLER'] = "haberdasher"
+      end
+
+      it "return proper logger class" do
+        expect(subject.class).to eq(Insights::Loggers::StdErrorLogger)
+      end
+    end
+  end
+end